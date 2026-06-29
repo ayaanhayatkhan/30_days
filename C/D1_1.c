0082c4e9adb0c0e8561cd07fbad8f0dd8ee593d8
AyaanHayatKhan <ayaanhayatkhan009@gmail.com>
2026-06-29 21:07:27 +0500

    Resolving the errors

--- C/D1_1.c
+++ C/D1_1.c
@@ -3,6 +3,7 @@ int main()
 {
 int var = 20;
 
+
 printf("var = %d\n", var);
 return 0;
 
--- C/D1_5.c
+++ C/D1_5.c
@@ -7,7 +7,8 @@ int main()
     printf("The size of int: %d\n", sizeof(int));
     printf("The size of char: %d\n", sizeof(char));
     printf("The size of float: %d\n", sizeof(float));
-    printf("The size of double: %d", sizeof(double));
-
+    printf("The size of double: %d\n", sizeof(double));
+   // printf("The size of memory: %d\n",sizeof(memory));
+    printf("The largest memory in these all is of Double\n");
     return 0;
 }
